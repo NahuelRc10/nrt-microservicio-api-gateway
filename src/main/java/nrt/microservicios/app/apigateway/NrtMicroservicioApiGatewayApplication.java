224438d7d67402987a29026be7d699bb170d33ee
NahuelRc10 <nahueltabasso@gmail.com>
2020-04-02 23:03:08 -0300

    PRIMER COMMIT

---
+++ src/main/java/nrt/microservicios/app/apigateway/NrtMicroservicioApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package nrt.microservicios.app.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class NrtMicroservicioApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrtMicroservicioApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nrt/microservicios/app/apigateway/NrtMicroservicioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package nrt.microservicios.app.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NrtMicroservicioApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
