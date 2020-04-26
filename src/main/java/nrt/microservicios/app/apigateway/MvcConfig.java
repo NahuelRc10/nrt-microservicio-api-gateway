7ca9ac768a81950d9ba4f8e0ea24a07399528ad7
NahuelRc10 <nahueltabasso@gmail.com>
2020-04-26 00:13:52 -0300

    Configuracion de CORS

---
+++ src/main/java/nrt/microservicios/app/apigateway/MvcConfig.java
@@ -0,0 +1,22 @@
+package nrt.microservicios.app.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://localhost:4200", "*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+	
+}
